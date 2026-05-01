ddca503473ff5ea2eedba9d7134b8b5c506aa6fa
gamerps0720-coder <gamerps0720@gmail.com>
2026-05-01 01:48:31 -0400

    Reorganize create function for clarity

--- game.js
+++ game.js
@@ -8,12 +8,21 @@ function preload() {
     // We will put images here later
 }
 function create() {
-    // This sets the sky color
+    // 1. Set the sky color
     this.cameras.main.setBackgroundColor('#87CEEB');
-  
-  // 2. Create the player (Red Square)
-    this.player = this.add.rectangle(400, 100, 50, 50, 0xff0000);
 
-    // 3. Turn on the physics for the player
+    // 2. Create the "Static" Floor (Green Rectangle)
+    this.platforms = this.physics.add.staticGroup();
+    let floor = this.add.rectangle(400, 580, 800, 40, 0x00ff00);
+    
+    // 3. Make the floor solid
+    this.physics.add.existing(floor, true); 
+    this.platforms.add(floor);
+
+    // 4. Create the player (Red Square)
+    this.player = this.add.rectangle(400, 100, 50, 50, 0xff0000);
     this.physics.add.existing(this.player);
+
+    // 5. THE MAGIC: This prevents falling through the floor
+    this.physics.add.collider(this.player, this.platforms);
 }
