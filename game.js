739f44985cf1ffe657e4c05e6516a48353de3dbb
gamerps0720-coder <gamerps0720@gmail.com>
2026-05-01 02:07:50 -0400

    Refactor game.js for better physics and controls
    Updated game physics and player controls for improved gameplay experience.

--- game.js
+++ game.js
@@ -1,54 +1,63 @@
 const config = {
-    type: Phaser.AUTO, // Added this for safety
-    width: 800, 
+    type: Phaser.AUTO,
+    width: 800,
     height: 600,
     physics: {
-        default: 'arcade', 
-        arcade: { gravity: { y: 300 } } 
+        default: 'arcade',
+        arcade: { gravity: { y: 600 } } // Higher gravity for a "snappier" feel
     },
-    // FIX 1: Added update to the scene list here!
     scene: { preload: preload, create: create, update: update }
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-    // Images coming soon!
-}
+function preload() {}
 
 function create() {
     this.cameras.main.setBackgroundColor('#87CEEB');
 
+    // 1. Setup the World Size (5000 pixels wide!)
+    this.physics.world.setBounds(0, 0, 5000, 600);
+    this.cameras.main.setBounds(0, 0, 5000, 600);
+
     this.platforms = this.physics.add.staticGroup();
-    let floor = this.add.rectangle(400, 580, 800, 40, 0x00ff00);
-    this.physics.add.existing(floor, true); 
+
+    // 2. Create the starting floor
+    let floor = this.add.rectangle(2500, 580, 5000, 40, 0x00ff00);
+    this.physics.add.existing(floor, true);
     this.platforms.add(floor);
 
-    this.player = this.add.rectangle(400, 100, 50, 50, 0xff0000);
-    this.physics.add.existing(this.player);
-    
-    // FIX 2: Tell Phaser to listen to the keyboard
-    this.cursors = this.input.keyboard.createCursorKeys();
+    // 3. PROCEDURAL GENERATION: The Platform Loop
+    // Every 400 pixels, we place a random floating platform
+    for (let i = 400; i < 5000; i += 400) {
+        let randomY = Phaser.Math.Between(200, 450); // Random height
+        let plat = this.add.rectangle(i, randomY, 200, 20, 0x00ff00);
+        this.physics.add.existing(plat, true);
+        this.platforms.add(plat);
+    }
 
+    // 4. Create the Player
+    this.player = this.add.rectangle(100, 450, 50, 50, 0xff0000);
+    this.physics.add.existing(this.player);
+    this.player.body.setCollideWorldBounds(true); // Don't let him leave the map!
+
+    // 5. CAMERA FOLLOW
+    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
     this.physics.add.collider(this.player, this.platforms);
 }
 
 function update() {
-    // If the left arrow is down, move left
     if (this.cursors.left.isDown) {
-        this.player.body.setVelocityX(-160);
-    }
-    // If the right arrow is down, move right
-    else if (this.cursors.right.isDown) {
-        this.player.body.setVelocityX(160);
-    }
-    // If nothing is pressed, stop moving
-    else {
+        this.player.body.setVelocityX(-200);
+    } else if (this.cursors.right.isDown) {
+        this.player.body.setVelocityX(200);
+    } else {
         this.player.body.setVelocityX(0);
     }
 
-    // Jump if Up is pressed AND the square is on the floor
     if (this.cursors.up.isDown && this.player.body.touching.down) {
-        this.player.body.setVelocityY(-330);
+        this.player.body.setVelocityY(-500);
     }
 }
